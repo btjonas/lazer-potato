af0237b55417f2e165e9de0b04849f590ef6968f
Jonas <grillkorv@gmail.com>
2015-08-31 16:03:00 +0200

    changes to json

--- server.js
+++ server.js
@@ -3,21 +3,37 @@ var beaconBadRequest = { beaconidfound: false };
 
 // Avaliable beacons and the equipment they are aware of, minor numbers are used to request equipment for a specific beacon
 var beacon3823 = { beaconidfound: true, beaconname: "Jonas1", equipment: 
-	[{ name: 'Garbage Can' , status: 'fully functional' , service: 'no not needed', picture: 'placeholder', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] },
-	{ name: 'Garage Door Opener' , status: 'operational' , service: '2 months until service', picture: 'placeholder', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }] };
+	[{ name: 'Garbage Can' , status: 'fully functional' , service: 'no not needed', picture: 'placeholder' },
+	{ name: 'Garage Door Opener' , status: 'operational' , service: '2 months until service', picture: 'placeholder' }] };
 
 var beacon56235 = { beaconidfound: true, beaconname: "Jonas2", equipment: 
-	[{ name: 'Garbage Can' , status: 'fully functional' , service: 'no not needed', picture: 'placeholder', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] },
-	{ name: 'Garage Door Opener' , status: 'operational' , service: '2 months until service', picture: 'placeholder', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }] };
+	[{ name: 'Garbage Can' , status: 'fully functional' , service: 'no not needed', picture: 'placeholder' },
+	{ name: 'Garage Door Opener' , status: 'operational' , service: '2 months until service', picture: 'placeholder' }] };
 
 var beacon7754 = { beaconidfound: true, beaconname: "Jonas3", equipment: 
-  [{ name: 'Output Valve' , status: 'Open' , service: 'no not needed', picture: 'valve', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] },
-  { name: 'Input Valve' , status: 'Open' , service: 'no not needed', picture: 'valve', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] },
-  { name: 'Asynchronous motor' , status: 'Inactive - 0 RPM' , service: '2 months until service', picture: 'async_motor', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }] }; 
+  [{ name: 'Output Valve' , status: 'Open' , service: 'no not needed', picture: 'valve' },
+  { name: 'Input Valve' , status: 'Open' , service: 'no not needed', picture: 'valve' },
+  { name: 'Asynchronous motor' , status: 'Inactive - 0 RPM' , service: '2 months until service', picture: 'async_motor' }] }; 
 
 var beacon30303 = { beaconidfound: true, beaconname: "Jonas4", equipment: 
-  [{ name: 'Asynchronous motor' , status: '4500 RPM' , service: 'no not needed', picture: 'async_motor', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] },
-  { name: 'Bipolar motor' , status: 'Inactive - 0 RPM' , service: '2 months until service', picture: 'bipolar_motor', functionality: [{ faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }] };   
+  [{ name: 'Asynchronous motor' , status: '4500 RPM' , service: 'no not needed', picture: 'async_motor' },
+  { name: 'Bipolar motor' , status: 'Inactive - 0 RPM' , service: '2 months until service', picture: 'bipolar_motor' }] };   
+
+var functionality3823 = { beaconidfound: true, functionality: 
+  [{faceplate: true, servicehistory: true, pid: true, processgraphics: true }, 
+  {faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }
+
+var functionality56235 = { beaconidfound: true, functionality: 
+  [{faceplate: true, servicehistory: true, pid: true, processgraphics: true }, 
+  {faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }
+
+var functionality7754 = { beaconidfound: true, functionality: 
+  [{faceplate: true, servicehistory: true, pid: true, processgraphics: true }, 
+  {faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }  
+
+var functionality30303 = { beaconidfound: true, functionality: 
+  [{faceplate: true, servicehistory: true, pid: true, processgraphics: true }, 
+  {faceplate: true, servicehistory: true, pid: true, processgraphics: true }] }
 
 var express = require('express');
 var app = express();
@@ -27,7 +43,7 @@ var app = express();
 // Unvalid requests will return string 
 app.get('/beaconid', function (req, res) {
   console.log('INFO - beacon id requested with minor: ' + req.query.id);
-  var requestedID = "beacon" + req.query.id
+  var requestedID = "beacon" + req.query.id;
 
   try {
     res.json(eval(requestedID));
@@ -36,6 +52,19 @@ app.get('/beaconid', function (req, res) {
     res.json(beaconBadRequest);
     console.log("BAD - ReferenceError: " + req.query.id + " is not defined"); 
   }		
+});
+
+app.get('/functionalityid', function (req, res) {
+  console.log('INFO - functionality id requested with minor: ' + req.query.id);
+  var requestedFunctionality = "functionality" + req.query.id;
+
+  try {
+    res.json(eval(requestedFunctionality));
+    console.log("OK - Request for beacon functionality: " + req.query.id + " has been processed"); 
+  } catch (e) {
+    res.json(beaconBadRequest)
+    console.log("BAD - ReferenceError: " + req.query.id + " is not defined"); 
+  }
 });
 
 // Depricated, a really silly way of using GET witouth using query string data
